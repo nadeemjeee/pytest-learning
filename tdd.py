e6b06a83d03bcd1f0b366bde3dc82eb9a93e5050
Nadeem Ahmad <nadeemjeee@gmail.com>
2026-04-17 18:46:31 +0200

    added subtract method in TDD

--- tdd.py
+++ tdd.py
@@ -1,3 +1,13 @@
 class Calculator:
     def add (self,a,b):
         return 7
+    
+    def add (self,a,b):
+        return a+b
+    
+    def substract (self,a,b):
+        return 3
+    
+    def substract (self,a,b):
+        return (a-b)
+
--- test_tdd.py
+++ test_tdd.py
@@ -3,3 +3,7 @@ from tdd import Calculator
 def test_add():
     calc = Calculator()
     assert calc.add (3,4)==7
+
+def test_substract():
+    calc = Calculator()
+    assert calc.substract(6,3)==3
